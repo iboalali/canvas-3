bef1a96805708b9e8ceba03ec2a54548f610e166
Ibrahim Al-Alali <iboalali@gmail.com>
2020-08-02 21:33:17 +0200

    finished implmenting
    one bug is still there. if an (أ) has a diacritics, it will not render correctly

--- app/src/main/java/de/ichexample/canvas_3/MyCanvasView2.java
+++ app/src/main/java/de/ichexample/canvas_3/MyCanvasView2.java
@@ -4,12 +4,11 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.res.ResourcesCompat;
 
@@ -21,15 +20,15 @@ import java.util.Locale;
 class MyCanvasView2 extends View {
 
     private Paint mTextPaint;
-    private int margin;
-    private int linePadding;
+    private Paint mDiacriticsPaint;
+    private int mMargin;
+    private int mLinePadding;
     @ColorInt
-    private int backgroundColor;
-    Rect textBound = new Rect();
-    String[] lines = new String[]{
+    private int mBackgroundColor;
+    String[] mLines = new String[]{
             "ها هو هي",
             "ه ههه",
-            "ه هاء",
+            "بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ",
             "هادي نَهْر",
             "كَتَب واجِباته",
             "حَيَوانٌ جَميلٌ",
@@ -52,50 +51,65 @@ class MyCanvasView2 extends View {
         init();
     }
 
+    public MyCanvasView2(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
     private void init() {
         mTextPaint = new Paint(Paint.LINEAR_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG);
         mTextPaint.setTextAlign(Paint.Align.RIGHT);
         mTextPaint.setTextSize(getResources().getDimension(R.dimen.textSize));
         mTextPaint.setColor(Color.BLACK);
-        Locale arabicLocale = new Locale("ar", "JO", "");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            mTextPaint.setTextLocale(arabicLocale);
-        }
+        mTextPaint.setTextLocale(new Locale("ar", "JO", ""));
 
-        backgroundColor = ResourcesCompat.getColor(getResources(), R.color.mycolor, null);
-        linePadding = getResources().getDimensionPixelOffset(R.dimen.line_padding);
-        margin = getResources().getDimensionPixelOffset(R.dimen.default_margin);
+        mDiacriticsPaint = new Paint(Paint.LINEAR_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG);
+        mDiacriticsPaint.setTextAlign(Paint.Align.RIGHT);
+        mDiacriticsPaint.setTextSize(getResources().getDimension(R.dimen.textSize));
+        mDiacriticsPaint.setColor(Color.RED);
+        mDiacriticsPaint.setTextLocale(new Locale("ar", "JO", ""));
+
+        mBackgroundColor = ResourcesCompat.getColor(getResources(), R.color.mycolor, null);
+        mLinePadding = getResources().getDimensionPixelOffset(R.dimen.line_padding);
+        mMargin = getResources().getDimensionPixelOffset(R.dimen.default_margin);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawColor(backgroundColor);
+        canvas.drawColor(mBackgroundColor);
 
         // the methods getWidth() and getTop() can only give the correct value after the
         // initialization phase
-        int posX = getWidth() - margin;
-        int posY = margin + getTop();
+        int posY = mMargin + getTop();
 
-        for (String line : lines) {
+        for (String line : mLines) {
+            int posX = getWidth() - mMargin;
+
+            String lineWithoutArabicDiacritics = removeArabicDiacriticsFromString(line);
+
             // This method gets you the boundaries of the given text.
             // This is not needed if we draw the whole line at once.
             //mTextPaint.getTextBounds(line, 0, line.length(), textBound);
 
-            canvas.drawText(line, posX, posY, mTextPaint);
-            posY += linePadding;
-        }
-        /* drawing char for char
-        for (String line : lines) {
-            int posX = getWidth() - margin;
-            for (int i = 0; i < line.length(); i++) {
-                // This method gets you the boundaries of the given text.
-                mTextPaint.getTextBounds(line, i, (i + 1), textBound);
-                posX -= (textBound.right - textBound.left);
-                canvas.drawText(line, i, (i + 1), posX, posY, mTextPaint);
+            // skip drawing the arabic text in red, if there are no diacritics in this line, to
+            // improve performance.
+            if (!lineWithoutArabicDiacritics.equals(line)) {
+                canvas.drawTextRun(line, 0, line.length(), 0, line.length(), posX, posY, true, mDiacriticsPaint);
             }
-            posY += linePadding;
+
+            canvas.drawTextRun(lineWithoutArabicDiacritics, 0, lineWithoutArabicDiacritics.length(), 0, lineWithoutArabicDiacritics.length(), posX, posY, true, mTextPaint);
+            posY += mLinePadding;
         }
-         */
+    }
+
+    private String removeArabicDiacriticsFromString(@NonNull String line) {
+        String[] diacritics = getResources().getStringArray(R.array.diacritics);
+
+        for (String diacritic : diacritics) {
+            line = line.replace(diacritic, "");
+        }
+
+        return line;
     }
 }
